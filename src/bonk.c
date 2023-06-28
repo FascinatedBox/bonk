e9044654c45080938eb2b577f5362779e680a50e
FascinatedBox <FascinatedBox@protonmail.com>
2023-06-28 13:51:46 -0700

    Move dispatch table into a file by itself.

--- src/bonk.c
+++ src/bonk.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 #include "bonk_internal.h"
+#include "bonk_dispatch_table.h"
 
 bonk_state_t *bonk_new_state(int argc_, char **argv_)
 {
@@ -45,54 +46,6 @@ void bonk_free_state(bonk_state_t *b)
     xcb_disconnect(conn);
     free(b);
 }
-
-typedef int (*bonk_func)(bonk_state_t *);
-
-typedef struct {
-    char *name;
-    bonk_func dispatch_func;
-} bonk_dispatch_entry;
-
-extern int b_activate(bonk_state_t *);
-extern int b_close(bonk_state_t *);
-extern int b_decoration(bonk_state_t *);
-extern int b_focus(bonk_state_t *);
-extern int b_get_active(bonk_state_t *);
-extern int b_get_focus(bonk_state_t *);
-extern int b_get_title(bonk_state_t *);
-extern int b_help(bonk_state_t *);
-extern int b_lower(bonk_state_t *);
-extern int b_map(bonk_state_t *);
-extern int b_move_resize(bonk_state_t *);
-extern int b_pick(bonk_state_t *);
-extern int b_prop_adjust(bonk_state_t *);
-extern int b_prop_delete(bonk_state_t *);
-extern int b_raise(bonk_state_t *);
-extern int b_select(bonk_state_t *);
-extern int b_state(bonk_state_t *);
-extern int b_unmap(bonk_state_t *);
-
-static bonk_dispatch_entry dispatch_table[] = {
-    { "activate",             b_activate },
-    { "close",                b_close },
-    { "decoration",           b_decoration },
-    { "focus",                b_focus },
-    { "get-active",           b_get_active },
-    { "get-focus",            b_get_focus },
-    { "get-title",            b_get_title },
-    { "help",                 b_help },
-    { "lower",                b_lower },
-    { "map",                  b_map },
-    { "move-resize",          b_move_resize },
-    { "pick",                 b_pick },
-    { "prop-adjust",          b_prop_adjust },
-    { "prop-delete",          b_prop_delete },
-    { "raise",                b_raise },
-    { "select",               b_select },
-    { "state",                b_state },
-    { "unmap",                b_unmap },
-    { NULL,                   NULL },
-};
 
 int bonk_exec(bonk_state_t *b)
 {
