23902b9dd55202b0aa32d7850f8e58db18a98acd
FascinatedBox <FascinatedBox@protonmail.com>
2024-04-14 13:05:31 -0700

    Fix oops in `prop-adjust` test.

--- tests/t_cmd_prop_adjust_delete.sh
+++ tests/t_cmd_prop_adjust_delete.sh
@@ -7,7 +7,7 @@ xtrace -D :9 ./bonk select --instance xterm \
                     prop-adjust --instance xterm- \
                     prop-adjust --wait --instance xterm \
                     prop-adjust --title xterm- \
-                    prop-adjust --wid $WID --wait --title xterm
+                    prop-adjust --window $WID --wait --title xterm
 
 xtrace -D :9 ./bonk select --instance xterm \
                     prop-delete --wait WM_HINTS \
