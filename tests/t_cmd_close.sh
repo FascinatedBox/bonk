686c9185ae808252f2318b095a54f5b82316e4ec
FascinatedBox <FascinatedBox@protonmail.com>
2024-05-01 13:50:04 -0700

    Fix `close` and `decoration` tests not using xtrace.

--- tests/t_cmd_close.sh
+++ tests/t_cmd_close.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 WID=`./bonk select --instance xterm`
-./bonk select --instance xterm \
+xtrace -D :9 ./bonk select --instance xterm \
                     close \
                     close --wait -w $WID
 
--- tests/t_cmd_decoration.sh
+++ tests/t_cmd_decoration.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 WID=`./bonk select --instance xterm`
-./bonk select --instance xterm \
+xtrace -D :9 ./bonk select --instance xterm \
                     decoration none \
                     decoration --wait maximize \
                     decoration -w $WID resize,maximize,minimize,close \
