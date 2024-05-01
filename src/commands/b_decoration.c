0a78836d4fffe3bd77e297744ee17be83279de79
FascinatedBox <FascinatedBox@protonmail.com>
2024-05-01 15:30:01 -0700

    `decoration`: Fix incorrect ideas, set functions as-needed.
    Prior to this patch, decorations and functions were muddled together and
    sent together. I'm not sure why I thought that was at all correct.
    
    This implementation correctly allows for all forms of decoration.
    However, it leaves functions alone. This means that decorations can be
    restricted, but functions cannot be. Which, in turn, means that
    `decoration none` will result in an undecorated window that can be moved
    and resized (assuming wm support for alt+mouse clicking).
    
    I'm open to allowing this command to explicitly specify functions as
    well, but I'll save that for when a sufficient use case is provided. I
    like being able to move and resize my borderless windows.

--- src/commands/b_decoration.c
+++ src/commands/b_decoration.c
@@ -4,12 +4,24 @@
 
 #include "bonk_internal.h"
 
-#define DECORATION_ALL (1L << 0)
-#define DECORATION_RESIZE (1L << 1)
-#define DECORATION_MOVE (1L << 2)
-#define DECORATION_MINIMIZE (1L << 3)
-#define DECORATION_MAXIMIZE (1L << 4)
-#define DECORATION_CLOSE (1L << 5)
+/* Mostly unused, but added for completeness. */
+#define MWM_FUNC_ALL       (1L << 0)
+#define MWM_FUNC_RESIZE    (1L << 1)
+#define MWM_FUNC_MOVE      (1L << 2)
+#define MWM_FUNC_MINIMIZE  (1L << 3)
+#define MWM_FUNC_MAXIMIZE  (1L << 4)
+#define MWM_FUNC_CLOSE     (1L << 5)
+
+#define MWM_DECOR_ALL      (1L << 0)
+#define MWM_DECOR_BORDER   (1L << 1)
+#define MWM_DECOR_RESIZEH  (1L << 2)
+#define MWM_DECOR_TITLE    (1L << 3)
+#define MWM_DECOR_MENU     (1L << 4)
+#define MWM_DECOR_MINIMIZE (1L << 5)
+#define MWM_DECOR_MAXIMIZE (1L << 6)
+
+#define HINT_FUNCTIONS (1L << 0)
+#define HINT_DECORATIONS (1L << 1)
 
 typedef enum {
     opt_wait,
@@ -35,14 +47,12 @@ static const char *usage =
     "\n"
     "Set a window's decorations to only include 'decorations'.\n"
     "Valid decorations are:\n"
-    "   all, none, close, maximize, minimize, move, resize\n"
+    "   all, none, border, close, maximize, minimize, resize, title\n"
     "\n"
     "Decorations must be given as a single comma-separated string:\n"
-    "   decoration resize,move,close\n"
+    "   decoration resize,close\n"
     ;
 
-/* I have no idea what's going here, I just know enough to get motif to adjust
-   window decorations. */
 typedef struct {
     uint32_t flags;
     uint32_t functions;
@@ -51,20 +61,23 @@ typedef struct {
     uint32_t status;
 } bonk_motif_wm_hints;
 
-#define HINT_FUNCTIONS (1L << 0)
-#define HINT_DECORATIONS (1L << 1)
-
 static void do_set_motif_wm_hints(bonk_state_t *b,
                                   xcb_window_t window,
                                   xcb_atom_t motif_atom,
+                                  uint32_t functions,
                                   uint32_t decorations)
 {
     bonk_motif_wm_hints hints;
 
     memset(&hints, 0, sizeof(hints));
-    hints.flags = HINT_FUNCTIONS | HINT_DECORATIONS;
+
+    hints.flags = HINT_DECORATIONS;
     hints.decorations = decorations;
-    hints.functions = decorations;
+
+    if (functions != UINT32_MAX) {
+        hints.flags |= HINT_FUNCTIONS;
+        hints.functions = functions;
+    }
 
     xcb_change_property(b->conn,
                         XCB_PROP_MODE_REPLACE,
@@ -76,38 +89,47 @@ static void do_set_motif_wm_hints(bonk_state_t *b,
                         (char *)&hints);
 }
 
-static uint32_t parse_decorations(char *decoration_str)
+static uint32_t parse_decorations(char *decoration_str,
+                                  uint32_t *functions,
+                                  uint32_t *decorations)
 {
     char *tok, *save_ptr;
-    int decorations = 0;
+    uint32_t d = 0, funcs = UINT32_MAX;
 
     while ((tok = strtok_r(decoration_str, ",", &save_ptr))) {
         if (decoration_str != NULL)
             decoration_str = NULL;
 
         if (strcmp(tok, "none") == 0)
-            decorations = 0;
-        else if (strcmp(tok, "resize") == 0)
-            decorations |= DECORATION_RESIZE;
-        else if (strcmp(tok, "minimize") == 0)
-            decorations |= DECORATION_RESIZE | DECORATION_MINIMIZE;
+            d = 0;
+        else if (strcmp(tok, "border") == 0)
+            d |= MWM_DECOR_BORDER;
         else if (strcmp(tok, "maximize") == 0)
-            decorations |= DECORATION_RESIZE | DECORATION_MAXIMIZE;
+            d |= MWM_DECOR_MAXIMIZE;
+        else if (strcmp(tok, "menu") == 0)
+            d |= MWM_DECOR_MENU;
+        else if (strcmp(tok, "minimize") == 0)
+            d |= MWM_DECOR_MINIMIZE;
+        else if (strcmp(tok, "resize") == 0)
+            d |= MWM_DECOR_RESIZEH;
+        else if (strcmp(tok, "title") == 0)
+            d |= MWM_DECOR_TITLE;
         else if (strcmp(tok, "close") == 0)
-            decorations |= DECORATION_RESIZE | DECORATION_CLOSE;
+            funcs = MWM_FUNC_ALL | MWM_FUNC_CLOSE;
         else if (strcmp(tok, "all") == 0)
-            decorations = DECORATION_RESIZE |
-                          DECORATION_MOVE |
-                          DECORATION_MAXIMIZE |
-                          DECORATION_MINIMIZE |
-                          DECORATION_CLOSE;
+            d = MWM_DECOR_ALL;
         else {
             fprintf(stderr, "bonk decoration error: Invalid decoration '%s'.\n", tok);
-            return UINT32_MAX;
+            d = UINT32_MAX;
+            break;
         }
     }
 
-    return decorations;
+    if (funcs == UINT32_MAX)
+        funcs = MWM_FUNC_ALL;
+
+    *decorations = d;
+    *functions = funcs;
 }
 
 int b_decoration(bonk_state_t *b)
@@ -122,7 +144,9 @@ int b_decoration(bonk_state_t *b)
 
     bonk_arg_window_and_require_n(b, 1);
 
-    uint32_t decorations = parse_decorations(bonk_arg_next_unchecked(b));
+    uint32_t funcs, decorations;
+    parse_decorations(bonk_arg_next_unchecked(b), &funcs, &decorations);
+
     xcb_atom_t motif_atom = bonk_atom_find_or_intern(b->conn,
             "_MOTIF_WM_HINTS");
 
@@ -130,7 +154,7 @@ int b_decoration(bonk_state_t *b)
         return 0;
 
     BONK_FOREACH_WINDOW_DO(
-        do_set_motif_wm_hints(b, iter_window, motif_atom, decorations);
+        do_set_motif_wm_hints(b, iter_window, motif_atom, funcs, decorations);
     )
 
     if (wait)
--- tests/t_cmd_decoration.sh
+++ tests/t_cmd_decoration.sh
@@ -3,8 +3,9 @@
 WID=`./bonk select --instance xterm`
 xtrace -D :9 ./bonk select --instance xterm \
                     decoration none \
-                    decoration --wait maximize \
+                    decoration --wait close \
                     decoration -w $WID resize,maximize,minimize,close \
-                    decoration all
+                    decoration all \
+                    decoration -w $WID border,close,maximize,menu,minimize,resize,title \
 
 exit $?
