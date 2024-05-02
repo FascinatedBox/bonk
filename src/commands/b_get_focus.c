b1a3b52b0a1da80304bde911a345aeced64690a1
FascinatedBox <FascinatedBox@protonmail.com>
2024-05-01 20:27:31 -0700

    `get-focus`: If PointerRoot is encountered, send back the proper root.

--- src/commands/b_get_focus.c
+++ src/commands/b_get_focus.c
@@ -21,6 +21,38 @@ static const char *usage =
     "-h, --help               display this help and exit\n"
     ;
 
+static xcb_window_t get_pointer_root(bonk_state_t *b)
+{
+    xcb_screen_t **screens = b->ewmh->screens;
+    int nb_screens = b->ewmh->nb_screens;
+    xcb_window_t result = screens[0]->root;
+
+    if (nb_screens == 1)
+        return result;
+
+    for (int i = 0;i < nb_screens;i++) {
+        xcb_window_t root = screens[i]->root;
+
+        /* Play Go Fish with X to find out which screen the pointer is on. */
+        xcb_query_pointer_cookie_t c = xcb_query_pointer(b->conn, root);
+        xcb_query_pointer_reply_t *r = xcb_query_pointer_reply(b->conn, c,
+                NULL);
+
+        if (r == NULL)
+            continue;
+
+        if (r->same_screen) {
+            free(r);
+            result = root;
+            break;
+        }
+        else
+            free(r);
+    }
+
+    return result;
+}
+
 int b_get_focus(bonk_state_t *b)
 {
     BONK_GETOPT_LOOP(c, b, "+h", longopts) {
@@ -37,6 +69,13 @@ int b_get_focus(bonk_state_t *b)
 
     if (r) {
         xcb_window_t w = r->focus;
+
+        /* This can return None (0x0), PointerRoot (0x1), or a valid wid. The
+           middle one, PointerRoot, can be confusing to users since many X
+           applications will complain about an invalid wid. To prevent that,
+           figure out the right root and send that back. */
+        if (w == XCB_INPUT_FOCUS_POINTER_ROOT)
+            w = get_pointer_root(b);
 
         if (b->argc == 0)
             BONK_PRINT_WINDOW(w);
